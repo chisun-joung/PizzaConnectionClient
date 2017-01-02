910e9f3bf46aea1b125e21f677a24841e4b4356d
chisun-joung <csjoung@gmail.com>
2017-01-02 16:57:02 +0900

    p44

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,9 +4,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {InputTextModule, ButtonModule} from 'primeng/primeng';
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule,
+            InputTextModule,
+            ButtonModule,
+            FormsModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
